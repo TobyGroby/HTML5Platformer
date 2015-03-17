a09aab730f2f5c12bdb7db90096d7e61d4313e40
TobyGroby <tobygreen642@gmaill.com>
2015-03-17 19:25:58 +1100

    More Platformer files
    ...

--- main.js
+++ main.js
@@ -6,9 +6,20 @@ var endFrameMillis = Date.now();
 
 var player = new Player();
 var keyboard = new Keyboard();
+var LAYER_COUNT = 2;
+var MAP = {tw:60, th:15,};
+var TILE = 35
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
 
 
 
+	//load the image to use for the level tiles
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
 
 // This function will return the time in seconds since the function 
 // was last called
@@ -49,12 +60,39 @@ var fpsTime = 0;
 // load an image to draw
 var chuckNorris = document.createElement("img");
 chuckNorris.src = "hero.png";
+
+function drawMap()
+{
+	for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
+	{
+		var idx = 0;
+		for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+		{
+			for( var x = 0; x < level1.layers[layerIdx].width; x++ )
+			{
+				if( level1.layers[layerIdx].data[idx] != 0 )
+				{
+					// the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+					// correct tile
+					var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+					var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+					var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+					context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+				}
+				idx++;
+			}
+		}
+	}
+}
+
 function run ()
 {
 	context.fillStyle = "#ccc";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
 	var deltaTime = getDeltaTime();
+	
+	drawMap();
 	
 	player.update(deltaTime);
 	player.draw();
