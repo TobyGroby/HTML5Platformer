a707d63dbf5ef0902f44b8c4de2b6b98a2f56f7a
TobyGroby <tobygreen642@gmaill.com>
2015-03-19 20:31:18 +1100

    Platformer files

---
+++ Vector2.js
@@ -0,0 +1,34 @@
+Vector2.prototype.set = function (x,y)
+{
+	this.x = 0;
+	this.y = 0;
+};
+
+Vector2.prototype.normalize = function ()
+{
+	this.magnitude = (dirX * dirX) + (dirY * dirY);
+	if (magnitude !=0)
+	{
+			var oneOverMag = 1 / Math.sqrt(magnitude);
+			dirX *= oneOverMag;
+			dirY *= oneOverMag;
+	}
+}
+
+Vector2.prototype.add = function (v2)
+{
+this.x += v2.x;
+this.y += v2.y;
+}
+
+Vector2.prototype.subtract = function (v2)
+{
+this.x -= v2.x;
+this.y -= v2.y;
+}
+
+Vector2.prototype.multiplyScalar = function (num)
+{
+this.x *= num.x;
+this.y *= num.y;
+}
--- player.js
+++ player.js
@@ -1,26 +1,89 @@
-var Player = function(){
+var Player = function()
+{
 	this.image = document.createElement("img");
-	this.x = canvas.width/2;
-	this.y = canvas.height/2;
+	this.position = new Vector2();
+	this.position.set( 9*TILE, 0*TILE );
 	this.width = 159;
 	this.height = 163;
-	
+	this.offset = new Vector2();
+	this.offset.set(-55,-87);
+	this.velocity = new Vector2();
+	this.falling = true;
+	this.jumping = false;
 	this.image.src = "hero.png";
 };
 
+
+
 Player.prototype.update = function(deltaTime)
 {
-	if( typeof(this.rotation) == "undefined" )
-		this.rotation = 0;		//Variable?
-		
-	if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
+	// we’ll insert code here later
+	
+	// calculate the new position and velocity:
+	this.position.y = Math.floor(this.position.y + (deltaTime * this.velocity.y));
+	this.position.x = Math.floor(this.position.x + (deltaTime * this.velocity.x));
+	this.velocity.x = bound(this.velocity.x + (deltaTime * ddx), -MAXDX, MAXDX);
+	this.velocity.y = bound(this.velocity.y + (deltaTime * ddy), -MAXDY, MAXDY);
+	if ((wasleft && (this.velocity.x > 0)) ||
+		(wasright && (this.velocity.x < 0)))
 	{
-		this.rotation -= deltaTime;
-	}
-	else
+		// clamp at zero to prevent friction from making us jiggle side to side
+		this.velocity.x = 0;
+}
+	
+	// collision detection
+	// Our collision detection logic is greatly simplified by the fact that the
+	// player is a rectangle and is exactly the same size as a single tile.
+	// So we know that the player can only ever occupy 1, 2 or 4 cells.
+	// This means we can short-circuit and avoid building a general purpose
+	// collision detection engine by simply looking at the 1 to 4 cells that
+	// the player occupies:
+	var tx = pixelToTile(this.position.x);
+	var ty = pixelToTile(this.position.y);
+	var nx = (this.position.x)%TILE; // true if player overlaps right
+	var ny = (this.position.y)%TILE; // true if player overlaps below
+	var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
+	var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
+	var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
+	var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
+	if (left)
+		ddx = ddx - ACCEL; // player wants to go left
+	else if (wasleft)
+		ddx = ddx + FRICTION; // player was going left, but not any more
+	if (right)
+		ddx = ddx + ACCEL; // player wants to go right
+	else if (wasright)
+		ddx = ddx - FRICTION; // player was going right, but not any more
+	if (jump && !this.jumping && !falling)
 	{
-		this.rotation += deltaTime;
+		ddy = ddy - JUMP; // apply an instantaneous (large) vertical impulse
+		this.jumping = true;
 	}
+	
+	// calculate the new position and velocity:
+	this.position.y = Math.floor(this.position.y + (deltaTime * this.velocity.y));
+	this.position.x = Math.floor(this.position.x + (deltaTime * this.velocity.x));
+	this.velocity.x = bound(this.velocity.x + (deltaTime * ddx), -MAXDX, MAXDX);
+	this.velocity.y = bound(this.velocity.y + (deltaTime * ddy), -MAXDY, MAXDY);
+
+
+	// we’ll insert code here later
+	// collision detection
+	// Our collision detection logic is greatly simplified by the fact that the
+	// player is a rectangle and is exactly the same size as a single tile.
+	// So we know that the player can only ever occupy 1, 2 or 4 cells.
+	// This means we can short-circuit and avoid building a general purpose
+	
+	// collision detection engine by simply looking at the 1 to 4 cells that
+	// the player occupies:
+	var tx = pixelToTile(this.position.x);
+	var ty = pixelToTile(this.position.y);
+	var nx = (this.position.x)%TILE; 	// true if player overlaps right
+	var ny = (this.position.y)%TILE; 	// true if player overlaps below
+	var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
+	var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
+	var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
+	var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
 }
 
 Player.prototype.draw = function()
